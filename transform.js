b302db156a743bc50d5c7f90e5260a84d375f94c
Omar Almatov <omar.almatov@berkeley.edu>
2022-02-07 18:38:16 -0800

    exported transformResponse

--- transform.js
+++ transform.js
@@ -1,31 +1,3 @@
-const response = [
-    {
-        id: 1293487,
-        name: 'KCRW', // radio station callsign
-        tracks: [{ timestp: '2021-04-08', trackName: 'Peaches' }],
-    },
-    {
-        id: 12923,
-        name: 'KQED',
-        tracks: [
-            { timestp: '2021-04-09', trackName: 'Savage' },
-            { timestp: '2021-04-09', trackName: 'Savage (feat. Beyonce)' },
-            { timestp: '2021-04-08', trackName: 'Savage' },
-            { timestp: '2021-04-08', trackName: 'Savage' },
-            { timestp: '2021-04-08', trackName: 'Savage' },
-        ],
-    },
-    {
-        id: 4,
-        name: 'WNYC',
-        tracks: [
-            { timestp: '2021-04-09', trackName: 'Captain Hook' },
-            { timestp: '2021-04-08', trackName: 'Captain Hook' },
-            { timestp: '2021-04-07', trackName: 'Captain Hook' },
-        ],
-    },
-];
-
 const transformResponse = (res) => {
     /* 
         Function that takes in API response and transforms it into expected output.
@@ -122,4 +94,4 @@ const transformTracks = (tracks) => {
     return transformed.slice(0, -2);
 };
 
-console.log(transformResponse(response));
+exports.transformResponse = transformResponse;
